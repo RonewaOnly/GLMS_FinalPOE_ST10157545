538e8bfcedd618c913116d29e10a0fd82afb6ead
Student-Ronewa <st10157545@rcconnect.edu.za>
2026-06-13 23:46:22 +0200

    addition for the frontend code for the website

--- GLMS.Web/Controllers/HomeController.cs
+++ GLMS.Web/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
+using GLMS.Web.Data;
 using GLMS.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GLMS.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.TotalClients = await _context.Clients.CountAsync();
+            ViewBag.TotalContracts = await _context.Contracts.CountAsync();
+            ViewBag.ActiveContracts = await _context.Contracts.CountAsync(c => c.Status == Models.ContractStatus.Active);
+            ViewBag.ExpiredContracts = await _context.Contracts.CountAsync(c => c.Status == Models.ContractStatus.Expired);
+            ViewBag.TotalServiceRequests = await _context.ServiceRequests.CountAsync();
+            ViewBag.PendingRequests = await _context.ServiceRequests.CountAsync(sr => sr.Status == Models.ServiceRequestStatus.Pending);
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View();
     }
 }
